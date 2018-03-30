4e6415c37654d8a576940d6857c13e813366bb72
AzureAD\LUCASFERNANDODEASSIS <lucasaassis@outlook.com>
2018-03-30 13:04:16 -0300

    Adicionando estrutura inicial do projeto e layout inicial da sidenav

---
+++ SoftwareEstatistica.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SoftwareEstatistica.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
